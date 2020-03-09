cf719b39a9e62010cc7ab0f774b0b53cdb13d249
coderSau <mhasan.kzl@gmail.com>
2020-03-09 12:45:47 +0300

    ilk deneme

---
+++ src/test/java/com/kuzulu/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzulu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
